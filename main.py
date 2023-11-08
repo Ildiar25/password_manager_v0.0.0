3d1652f9f081a71aea4bed254bff4cf308a356ed
Joan <j.pastor1591@gmail.com>
2023-11-08 20:53:08 +0100

    Adding Principal Files
    Added first files to the project

---
+++ main.py
@@ -0,0 +1,12 @@
+###################################################   PRESENTATION   ###################################################
+
+# This is a litle project about Password Manager.
+# This is the version 0.0.0 where we are going to add the first steps to become a great and useful program.
+
+# On this section I'm going to explain the points where I'm working on. First of all, I'll start with some functions
+# and defying some variables to create the body program.
+
+########################################################################################################################
+
+import cryptography
+import os
