097efde03e7d598df735375b5d56f02032467f08
Joan <j.pastor1591@gmail.com>
2023-11-09 12:08:56 +0100

    Created First Encrypting File
    Added 'new_profile' function

---
+++ config.py
@@ -0,0 +1,3 @@
+temp_path = "C:/Users/jpast/Desktop/temp_folder/"
+
+PATH = temp_path
--- main.py
+++ main.py
@@ -8,5 +8,56 @@
 
 ########################################################################################################################
 
-import cryptography
+from cryptography.fernet import Fernet
 import os
+import time
+from config import PATH
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open(PATH+"key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open(PATH+"key.key", "rb").read()
+
+def data_encode(data):
+    data = data.encode()
+    key = load_key()
+    f = Fernet(key)
+    return f.encrypt(data)
+
+def data_decode(file):
+    key = load_key()
+    f = Fernet(key)
+    with open(file, "rb") as encrypt_data:
+        data = encrypt_data.read()
+    decrypted_data = f.decrypt(data)
+    return decrypted_data.decode()
+
+def new_profile(user_name):
+    print("Vamos a crear un nuevo usuario: ")
+    time.sleep(0.2)
+
+    open(f"{PATH}{user_name}_DATA.txt", "wb").close()
+
+    user = input("Nombre de Usuario: ")
+    password_01 = input("Contraseña: ")
+
+    while True:
+        password_02 = input("Repita la contraseña: ")
+
+        if password_02 == password_01:
+            print("Contraseña confirmada")
+            time.sleep(0.2)
+
+            user_data = user + "|" + password_01
+            user_encrypt = data_encode(user_data)
+
+            with open(f"{PATH}{user_name}_DATA.txt", "ab") as data:
+                data.write(user_encrypt)
+
+            break
+        else:
+            print("Lo siento, la contraseña no coincide.")
+            continue
