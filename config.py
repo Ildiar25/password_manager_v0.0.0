46f886573a4bc9e6f63ab80952da50567a4ded15
Joan <j.pastor1591@gmail.com>
2023-11-19 19:51:27 +0100

    Added Last Function
    Added a function to change user name or password and ending program body

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-NAME = "jpast"
+NAME = "Joan"
 BUILD = "v0.0.0"
 PATH = f"C:/Users/{NAME}/Password Manager/"
--- main.py
+++ main.py
@@ -21,7 +21,8 @@ content = os.listdir(f"C:/Users/{NAME}/")
 
 ####################### ------------------------------ FUNCTIONS ------------------------------ #######################
 
-# SISTEM FUNCTIONS ----
+
+# SYSTEM FUNCTIONS ----
 
 
 def install():
@@ -69,25 +70,25 @@ def uninstall():
             data_folder = PATH + "data/"
             files = os.listdir(data_folder)
 
-            for file in files:
-                print(file)
-                os.remove(data_folder + file)
+            for ufile in files:
+                print(ufile)
+                os.remove(data_folder + ufile)
 
             time.sleep(0.2)
             files_folder = PATH + "files/"
             files = os.listdir(files_folder)
 
-            for file in files:
-                print(file)
-                os.remove(files_folder + file)
+            for ufile in files:
+                print(ufile)
+                os.remove(files_folder + ufile)
 
             time.sleep(0.2)
             user_folder = PATH + "user/"
             files = os.listdir(user_folder)
 
-            for file in files:
-                print(file)
-                os.remove(user_folder + file)
+            for ufile in files:
+                print(ufile)
+                os.remove(user_folder + ufile)
 
             time.sleep(0.2)
 
@@ -116,6 +117,9 @@ def uninstall():
 
             os.rmdir(f"C:/Users/{NAME}/Password Manager")
             print("Finalización Exitosa")
+            time.sleep(0.2)
+
+            print(f"Gracias por utilizar Password Manager" + BUILD)
             quit()
 
         elif u_answer == "N":
@@ -123,6 +127,181 @@ def uninstall():
 
         else:
             print(f"Lo siento, {u_answer} no es un comando válido.")
+            continue
+
+
+def new_file(name_file):
+    print(f"Creación del elemento {name_file} en la biblioteca.")
+    time.sleep(0.2)
+    open(PATH + f"data/{name_file}.txt", "wb").close()
+
+    user = input("Nombre de Usuario: ")
+    password_01 = input("Contraseña: ")
+
+    while True:
+        password_02 = input("Repita la contraseña: ")
+
+        if password_02 == password_01:
+            print("Contraseña confirmada")
+            time.sleep(0.2)
+
+            user_encrypt = data_encode(user + "|" + password_01)
+
+            with open(PATH + f"data/{name_file}.txt", "ab") as data:
+                data.write(user_encrypt)
+
+            print(f"{name_file} añadido a la biblioteca con éxito.")
+
+            break
+
+        else:
+            print("Lo siento, la contraseña no coincide.")
+            continue
+
+
+def modify_file():
+    content_list = os.listdir(PATH + "data")
+    temp = []
+
+    for element in content_list:
+        name = element.split(".")
+        temp.append(name[0])
+
+    while True:
+        print("Qué archivo desea modificar?")
+        print(temp)
+        element = input().upper()
+
+        if element in temp:
+            load_data = PATH + f"data/{element}.txt"
+            l_data = data_decode(load_data)
+            l_data = l_data.split("|")
+            print("Usuario:", l_data[0])
+            print("Contraseña:", l_data[1])
+
+            while True:
+                print("¿Qué desea modificar?")
+                m_answer = input().upper()
+
+                if m_answer == "USUARIO":
+                    print("Introduzca el nuevo nombre de usuario:")
+                    l_data[0] = input()
+
+                    final_data = "|".join(l_data)
+
+                    new_user = data_encode(final_data)
+
+                    with open(PATH + f"data/{element}.txt", "wb") as nfile:
+                        nfile.write(new_user)
+
+                    print(f"Nombre de usuario de {element} actualizado.")
+                    time.sleep(1)
+                    break
+
+                elif m_answer == "CONTRASEÑA":
+                    print("Introduzca la nueva contraseña:")
+                    l_data[1] = input()
+
+                    final_data = "|".join(l_data)
+
+                    new_password = data_encode(final_data)
+
+                    with open(PATH + f"data/{element}.txt", "wb") as nfile:
+                        nfile.write(new_password)
+
+                    print(f"Contraseña de {element} actualizada.")
+                    time.sleep(1)
+                    break
+
+                elif m_answer == "ATRÁS" or m_answer == "ATRAS":
+                    break
+
+                else:
+                    print(f"Lo siento, {m_answer} no es un comando válido.")
+                    continue
+
+        elif element == "ATRÁS" or element == "ATRAS":
+            break
+
+        else:
+            print(f"Lo siento, {file} no se encuentra en la lista de archivos almacenados.")
+            continue
+
+
+def view_file():
+    content_list = os.listdir(PATH + "data")
+    temp = []
+
+    for element in content_list:
+        name = element.split(".")
+        temp.append(name[0])
+
+    while True:
+        print("¿Qué datos desea ver?")
+        print(temp)
+        v_answer = input().upper()
+
+        if v_answer in temp:
+            load_data = PATH + f"data/{v_answer}.txt"
+            l_data = data_decode(load_data)
+            l_data = l_data.split("|")
+
+            print("Usuario:", l_data[0])
+            print("Contraseña:", l_data[1])
+
+            time.sleep(10)
+
+        elif v_answer == "ATRÁS" or v_answer == "ATRAS":
+            break
+
+        else:
+            print(f"Lo siento, {v_answer} no se encuentra en la lista de archivos almacenados.")
+            continue
+
+
+def delete_file():
+
+    while True:
+        content_list = os.listdir(PATH + "data")
+        temp = []
+
+        for element in content_list:
+            name = element.split(".")
+            temp.append(name[0])
+
+        print("Ha seleccionado eliminar. ¿Que archivo desea eliminar de la lista siguiente?")
+        print(temp)
+        dfile = input().upper()
+
+        if dfile in temp:
+            print(f"Ha seleccionado eliminar {dfile}. ¿Desea proceder? Esta acción no se puede deshacer.")
+
+            while True:
+                d_answer = input("Y/N: ").upper()
+
+                if d_answer == "Y":
+                    print(f"{dfile} eliminado.")
+                    time.sleep(0.2)
+
+                    os.remove(PATH + f"data/{dfile}.txt")
+                    break
+
+                elif d_answer == "N":
+                    print("De acuerdo.")
+                    break
+
+                elif d_answer == "ATRÁS" or d_answer == "ATRAS":
+                    break
+
+                else:
+                    print(f"Lo siento, {d_answer} no es un comando válido.")
+                    continue
+
+        elif dfile == "ATRÁS" or dfile == "ATRAS":
+            break
+
+        else:
+            print(f"Lo siento, {dfile} no tiene un archivo asociado.")
             continue
 
 
@@ -149,11 +328,11 @@ def data_encode(data):
     return f.encrypt(data)
 
 
-def data_decode(file):
+def data_decode(dfile):
     key = load_key()
     f = Fernet(key)
 
-    with open(file, "rb") as encrypt_data:
+    with open(dfile, "rb") as encrypt_data:
         data = encrypt_data.read()
 
     decrypted_data = f.decrypt(data)
@@ -161,7 +340,7 @@ def data_decode(file):
     return decrypted_data.decode()
 
 
-# START FUNCTIONS ----
+# MAIN FUNCTIONS ----
 
 
 def check_list(name):
@@ -181,7 +360,7 @@ def new_profile(name):
     print("Vamos a crear un nuevo usuario: ")
     time.sleep(0.2)
 
-    open(f"{PATH}user/{name}_DATA.txt", "wb").close()
+    open(PATH + f"user/{name}_DATA.txt", "wb").close()
 
     n_user = input("Nombre de Usuario: ")
     password_01 = input("Contraseña: ")
@@ -193,12 +372,12 @@ def new_profile(name):
             print("Contraseña confirmada")
             time.sleep(0.2)
 
-            with open(f"{PATH}files/user_list.txt", "a") as data:
+            with open(PATH + "files/user_list.txt", "a") as data:
                 data.write(name + "|")
 
             user_encrypt = data_encode(n_user + "|" + password_01)
 
-            with open(f"{PATH}user/{name}_DATA.txt", "ab") as data:
+            with open(PATH + f"user/{name}_DATA.txt", "ab") as data:
                 data.write(user_encrypt)
 
             login(name)
@@ -212,7 +391,7 @@ def new_profile(name):
 
 def login(name):
     print("Inicie sesión rellenando los campos")
-    load_user = f"{PATH}user/{name}_DATA.txt"
+    load_user = PATH + f"user/{name}_DATA.txt"
 
     u_data = data_decode(load_user)
     u_data = u_data.split("|")
@@ -235,6 +414,58 @@ def login(name):
         else:
             print("Usuario o contraseña no coinciden. Vuelva a intentarlo.")
             continue
+
+
+def profile_update():
+    load_data = PATH + f"user/{user_name}_DATA.txt"
+    l_data = data_decode(load_data)
+    l_data = l_data.split("|")
+    print("Nombre de usuario:", l_data[0])
+    print("Contraseña:", l_data[1])
+
+    while True:
+        print("¿Qué desea modificar?")
+        m_answer = input().upper()
+
+        if m_answer == "NOMBRE" or m_answer == "USUARIO":
+            print("Introduzca el nuevo nombre de usuario:")
+            l_data[0] = input()
+
+            final_data = "|".join(l_data)
+
+            new_user = data_encode(final_data)
+
+            with open(PATH + f"user/{user_name}_DATA.txt", "wb") as nfile:
+                nfile.write(new_user)
+
+            print(f"Nombre de usuario de {user_name} actualizado.")
+            time.sleep(1)
+            login(user_name)
+            break
+
+        elif m_answer == "CONTRASEÑA":
+            print("Introduzca la nueva contraseña:")
+            l_data[1] = input()
+
+            final_data = "|".join(l_data)
+
+            new_password = data_encode(final_data)
+
+            with open(PATH + f"user/{user_name}_DATA.txt", "wb") as nfile:
+                nfile.write(new_password)
+
+            print(f"Contraseña de {user_name} actualizada.")
+            time.sleep(1)
+            login(user_name)
+            break
+
+        elif m_answer == "ATRÁS" or m_answer == "ATRAS":
+            break
+
+        else:
+            print(f"Lo siento, {m_answer} no es un comando válido.")
+            continue
+
 
 ######################## ------------------------------ PROGRAM ------------------------------ ########################
 
@@ -279,13 +510,30 @@ while True:
     answer = input(f"MENÚ PRINCIPAL: {user_name}, qué desea hacer? ").upper()
 
     if answer == "AÑADIR":
-        pass
+        print("Ha seleccionado AÑADIR elemento a la base de datos")
+        time.sleep(0.2)
+
+        file = input("Nombre del proveedor a almacenar: ").upper()
+        new_file(file)
+
     elif answer == "MODIFICAR":
-        pass
+        print("Ha seleccionado MODIFICAR datos de archivo")
+        time.sleep(0.2)
+
+        modify_file()
+
     elif answer == "VER":
-        pass
+        print("Ha seleccionado VER datos de un archivo")
+        time.sleep(0.2)
+
+        view_file()
+
     elif answer == "ELIMINAR":
-        pass
+        print("Ha seleccionado ELIMINAR datos")
+        time.sleep(0.2)
+
+        delete_file()
+
     elif answer == "AYUDA":
         print("Los comandos disponibles son:")
         time.sleep(0.2)
@@ -303,7 +551,11 @@ while True:
             answer = input(f"MENÚ OPCIONES: Qué desea hacer? ").upper()
 
             if answer == "USUARIO":
-                pass
+                print("Ha elegido actualizar los datos de usuario")
+                time.sleep(0.2)
+
+                profile_update()
+                break
 
             elif answer == "DESINSTALAR":
                 uninstall()
@@ -327,7 +579,7 @@ while True:
                 print("Los comandos disponibles son:")
                 time.sleep(0.2)
 
-                print("USUARIO: Modifica los datos del usuario")
+                print("USUARIO: Modifica los datos del usuario actual")
                 print("DESINSTALAR: Elimina el programa y los datos almacenados")
                 print("LOGOUT: Cambiar de usuario")
                 print("ATRÁS: Volver al menú principal")
